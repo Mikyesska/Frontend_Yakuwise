5ec49a350e9ab7fa4f66a24a92b11ea777062b52
Yessenia Huaman <yesel1601@hotmail.com>
2026-01-23 13:11:56 -0500

    [ADD] Conexión Angular - DRF

--- src/app/features/welcome/welcome-component/welcome-component.ts
+++ src/app/features/welcome/welcome-component/welcome-component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
-
+import { ApiService } from '../../../shared/services/api.service';
 @Component({
   selector: 'app-welcome-component',
   imports: [RouterLink, RouterLinkActive],
@@ -8,5 +8,18 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './welcome-component.scss',
 })
 export class WelcomeComponent {
-
+  clientes: any[] = [];
+  constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    this.apiService.get<any>('clientes').subscribe({
+      next: (data) => {
+        console.log('Clientes recibidos:', data);
+        this.clientes = data;
+        console.log('ehhhh!!!',this.clientes)
+      },
+      error: (err) => {
+        console.error('Error al obtener clientes', err);
+      },
+    });
+  }
 }
