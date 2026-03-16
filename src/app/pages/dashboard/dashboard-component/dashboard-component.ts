144789bfd838bc9ab31133df3bf5a5a36057aa3c
kaly <alcantarakaly@gmail.com>
2026-03-15 23:27:36 -0500

    [ADD] footer

--- src/app/pages/dashboard/dashboard-component/dashboard-component.ts
+++ src/app/pages/dashboard/dashboard-component/dashboard-component.ts
@@ -1,12 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { HeaderComponent } from "../../../core/layout/header/header-component/header-component";
-import { SidebarComponent } from "../../../core/layout/sidebar/sidebar-component/sidebar-component";
-import { FooterComponent } from "../../../core/layout/footer/footer-component/footer-component";
-import { MainLayoutComponent } from "../../../core/layout/main-layout/main-layout-component/main-layout-component";
 
 @Component({
   selector: 'app-dashboard-component',
-  imports: [HeaderComponent, SidebarComponent, FooterComponent, MainLayoutComponent],
+  imports: [],
   templateUrl: './dashboard-component.html',
   styleUrl: './dashboard-component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
